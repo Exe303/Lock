9297fb593572189caf5d1fd30c40b114c6c10bc7
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-03-14 13:53:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,31 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ############MDX NETWORK#############
+GitUser="perizinan" 
+# // MY IPVPS 
+export MYIP=$(curl -sS ipv4.icanhazip.com) 
+ 
+# // GETTING 
+VALIDITY () { 
+    today=date -d "0 days" +"%Y-%m-%d" 
+    Exp1=$(curl -sS https://raw.githubusercontent.com/kmardhex/izin/main/ipvps.conf | grep $MYIP | awk '{print $3}') 
+    if [[ $today < $Exp1 ]]; then 
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m" 
+    else 
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"; 
+    echo -e "\e[31mPlease renew your ipvps first\e[0m" 
+    exit 0 
+fi 
+} 
+IZIN=$(curl -sS https://raw.githubusercontent.com/kmardhex/izin/main/ipvps.conf | awk '{print $2}' | grep $MYIP) 
+if [ $MYIP = $IZIN ]; then 
+echo -e "\e[32mPermission Accepted...\e[0m" 
+VALIDITY 
+else 
+echo -e "\e[31mPermission Denied!\e[0m"; 
+echo -e "\e[31mPlease buy script first\e[0m" 
+exit 0 
+fi
 clear
 red='\e[1;31m'
 green='\e[0;32m'
